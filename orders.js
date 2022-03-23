cedfecd83248a43db0cb6d0b9918f3d2e574f51f
Henry Johnson <henryayomide8@gmail.com>
2022-03-23 13:15:56 -0400

    Initial commit

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '62634',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sony Playstation 5',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Apple MacBook Pro',
+        productNumber: '61725',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Nakamichi Surround wave Speakers',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'LogiTech 4k Ultra HD Camera',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '21193',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LED automatated lights',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
+
